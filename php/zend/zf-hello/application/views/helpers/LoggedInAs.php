12f1ca812fdd7f36cf4314d9264c4df86a870483
zhangchunsheng <zhangchunsheng@yongche.com>
2014-04-08 14:01:10 +0800

    add zend demo

---
+++ php/zend/zf-hello/application/views/helpers/LoggedInAs.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhang
+ * Date: 14-4-8
+ * Time: 下午12:21
+ */
+class Zend_View_Helper_LoggedInAs extends Zend_View_Helper_Abstract {
+    public function loggedInAs() {
+        $auth = Zend_Auth::getInstance();
+        if($auth -> hasIdentity()) {
+            $username = $auth -> getIdentity() -> username;
+            $logoutUrl = $this -> view -> url(array(
+                'controller' => 'auth',
+                'action' => 'logout'
+            ), null, true);
+            return 'Welcome ' . $username . '. <a href="'.$logoutUrl.'">Logout</a>';
+        }
+
+        $request = Zend_Controller_Front::getInstance() -> getRequest();
+        $controller = $request -> getControllerName();
+        $action = $request -> getActionName();
+        if($controller == 'auth' && $action == 'index') {
+            return '';
+        }
+        $loginUrl = $this -> view -> url(array(
+            'controller' => 'auth',
+            'action' => 'index'
+        ));
+        return '<a href="'.$loginUrl.'">Login</a>';
+    }
+}
