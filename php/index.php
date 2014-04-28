4a05ce66367e6fddd773c7365871f870f5b4240e
zhangchunsheng <zhangchunsheng423@gmail.com>
2014-04-28 11:58:21 +0800

    add argument demo

--- php/index.php
+++ php/index.php
@@ -1,7 +1,8 @@
 <?php
     echo get_include_path();
 
-    error_log("test", 3, "D:/wamp/logs/test");
+    date_default_timezone_set("Asia/Shanghai");
+    //error_log("test", 3, "D:/wamp/logs/test");
 
     $url = "";
     $header = "";
@@ -17,7 +18,7 @@
     $message = json_encode($message);
     $date = date("Ymd");
     //error_log($message, 3, "/var/log/httpd/coolpen_log-{$date}");
-    error_log("[" . date("Y-m-d H:i:s") . "] " . $message . "\r", 3, "D:/wamp/logs/coolpen_log-{$date}");
+    //error_log("[" . date("Y-m-d H:i:s") . "] " . $message . "\r", 3, "D:/wamp/logs/coolpen_log-{$date}");
 
     $url = "{USER}.yongche.org";
     $url = "testing.yongche.org";
@@ -25,4 +26,9 @@
     echo "test" . gettype(strpos($url, "{USER}"));
     if(strpos($url, "{USER}") > 0 || strpos($url, "{USER}") === 0) {
     	echo "please change the config";
+    }
+
+    test_func(array('limit'=>20));
+    function test_func($limit_arg = array('limit'=>10,'interval'=>10)) {
+        print_r($limit_arg);
     }
