a89d74c902350a2e8edd0bd0729f434412fb98b3
zhangchunsheng <zhangchunsheng423@gmail.com>
2020-04-08 18:14:19 +0800

    update

---
+++ java/spring/demo/src/test/java/com/luomor/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luomor.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
