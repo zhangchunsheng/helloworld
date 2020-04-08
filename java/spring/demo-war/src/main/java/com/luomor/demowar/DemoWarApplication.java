1f54ad9d1d226b0049535365530b8996cee75afa
zhangchunsheng <zhangchunsheng423@gmail.com>
2020-04-08 18:57:19 +0800

    update

---
+++ java/spring/demo-war/src/main/java/com/luomor/demowar/DemoWarApplication.java
@@ -0,0 +1,13 @@
+package com.luomor.demowar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoWarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoWarApplication.class, args);
+	}
+
+}
